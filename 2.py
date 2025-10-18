988071074b6bdc5bb291ab699b7d1cbcb6dc4d0b
aliceti51 <64bayram51@gmail.com>
2025-10-18 20:52:49 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,226 @@
+print("Hello, World!")
+print("Five is greater than two!")
+print("Python is fun!")
+print("Have a good day.")
+print("Learning Python is fun!")
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+print('This will also work!')
+print("Hello World!", end="")
+print("I will print on the same line.")
+print(3)
+print(358)
+print(50000)
+print(3 + 3)
+print(2 * 5)
+print("I am", 35, "years old.")
+x = 5
+y = "John"
+print(x)
+print(y)
+x = 4       # x is of type int
+x = "Sally" # x is now of type str
+print(x)
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
+a = 4
+A = "Sally"
+myvar = "John"
+my_var = "John"
+_my_var = "John"
+myVar = "John"
+MYVAR = "John"
+myvar2 = "John"
+
+myvar = "John"
+my-var = "John"
+
+myVariableName = "John"
+MyVariableName = "John"
+my_variable_name = "John"
+
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+x = 5
+y = 10
+print(x + y)
+
+x = "awesome"
+
+def myfunc():
+  print("Python is " + x)
+
+  x = "awesome"
+  
+  def myfunc():
+    x = "fantastic"
+    print("Python is " + x)
+  
+  myfunc()
+  
+  print("Python is " + x)
+
+  def myfunc():
+    global x
+    x = "fantastic"
+  
+  myfunc()
+  
+  print("Python is " + x)
+
+x = "Hello World"
+
+print(x)
+
+print(type(x)) 
+
+x = 20
+
+print(x)
+
+print(type(x)) 
+
+x = ["apple", "banana", "cherry"]
+
+print(x)
+
+print(type(x)) 
+
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = 3+5j
+y = 5j
+z = -5j
+
+print(type(x))
+print(type(y))
+print(type(z))
+
+x = int(1)
+y = int(2.8)
+z = int("3")
+print(x)
+print(y)
+print(z)
+
+x = float(1)
+y = float(2.8)
+z = float("3")
+w = float("4.2")
+print(x)
+print(y)
+print(z)
+print(w)
+
+x = str("s1")
+y = str(2)
+z = str(3.0)
+print(x)
+print(y)
+print(z)
+
+print('He is called "Johnny"')
+
+a = "Hello"
+print(a)
+
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+
+for x in "banana":
+  print(x)
+
+a = "Hello, World!"
+print(len(a))
+
+txt = "The best things in life are fre!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+print("free" in txt)
+
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+  txt = "The best things in life are free!"
+  print("expensive" not in txt)
+
+  txt = "The best things in life are free!"
+  if "expensive" not in txt:
+    print("No, 'expensive' is NOT present.")
+
+b = "Hello, World!"
+print(b[2:5])
+
+b = "Hello, World!"
+print(b[:5])
+
+b = "Hello, World!"
+print(b[2:])
+
+b = "Hello, World!"
+print(b[-5:-2])
+
+a = "Hello, World!"
+print(a.upper())
+
+a = "Hello, World!"
+print(a.lower())
+
+a = " Hello, World! "
+print(a.strip())
+
+a = "Hello, World!"
+print(a.replace("H", "T"))
+
+a = "Hello, World!"
+b = a.split(",")
+print(b)
+
+a = "Hello"
+b = "World"
+c = a + b
+print(c)
+
+a = "Hello"
+b = "World"
+c = a + " " + b
+print(c)
+
+age = 36
+txt = f"My name is John, I am {age}"
+print(txt)
+
+price = 59
+txt = f"The price is {price} dollars"
+print(txt)
+
+price = 59.01
+txt = f"The price is {price:.2f} dollars"
+print(txt)
+
+txt = f"The price is {20 * 59} dollars"
+print(txt)
